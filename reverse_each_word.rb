a9c90c52e65b65551f4596f5397513cfc3a7653b
oliviatian27@gmail.com <oliviatian27@gmail.com>
2018-07-22 22:37:39 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,16 @@
+def reverse_each_word(array)
+  array=array.split(' ')
+  result=[]
+  array.each do |x|
+    result<< x.reverse
+  end
+  result.join(' ')
+end
+
+def reverse_each_word(array)
+   array=array.split(' ')
+   result=array.collect do |x|
+    x.reverse
+  end
+  result.join(' ')
+end
